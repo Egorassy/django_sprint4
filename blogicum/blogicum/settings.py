cf599d7766cd961f38d6021992381d51bafb4c22
Egor Evseev <egor.evseev1312@gmail.com>
2025-12-28 23:30:29 +0400

    Debug False

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!r1pr&4fy^%xw$klkq8f#zr6^wmj1-wnjez=x#uw547ifu7%-9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
